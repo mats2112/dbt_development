11ff61e635070a740be7bc125a6a33109a4993e1
mats2112 <mats2112@gmail.com>
2025-01-10 17:49:52 +0530

    update model

--- dbt-coreProj/MyProjtest/models/Normal_Load.sql
+++ dbt-coreProj/MyProjtest/models/Normal_Load.sql
@@ -1,42 +1,13 @@
-<<<<<<< HEAD
+-- models/load_and_transform_data.sql
 
-  
-    
+with raw_data as (
+    select *
+    from "myprojtest-439206.Test.test2"
+)
 
-    create or replace table `myprojtest-439206`.`Test_Test`.`test2`
-      
-    
-    
-
-    OPTIONS()
-    as (
-      
-=======
-{{ config(
-    materialized='table',
-    schema='Test',
-    alias='test2',
-    pre_hook="
-        -- Load data from GCS into BigQuery using native BigQuery syntax
-        LOAD DATA INTO `myprojtest-439206.Test.test2`
-        FROM FILES (
-            format = 'CSV',
-            uris = ['gs://ms_source/Test_Data.csv'],
-            skip_leading_rows = 1  -- Skip the header row (if your CSV has headers)
-        );
-    "
-) }}
->>>>>>> e71fba5a2823738e602a317d3961a8bba28a4e63
-
--- Define a simple query to select data after it's been loaded
-SELECT * FROM `myprojtest-439206.Test.test2`
-
-<<<<<<< HEAD
--- 
-    );
-  
-=======
--- {% if is_incremental() %}
--- WHERE your_unique_key NOT IN (SELECT your_unique_key FROM {{id}})
--- {% endif %}
->>>>>>> e71fba5a2823738e602a317d3961a8bba28a4e63
+-- Transform the raw data as needed
+select
+    column1,
+    column2,
+    column3
+from test2

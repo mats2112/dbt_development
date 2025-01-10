a370fbf74ff4122bff0f629ca79c8f52a2948161
mats2112 <mats2112@gmail.com>
2025-01-10 15:23:05 +0530

    update in model folder

--- dbt-coreProj/MyProjtest/target/compiled/MyProjtest/models/Normal_Load.sql
+++
@@ -1,6 +0,0 @@
-
-
--- Define a simple query to select data after it's been loaded
-SELECT * FROM `myprojtest-439206.Test.test2`
-
--- 
--- dbt-coreProj/MyProjtest/target/compiled/MyProjtest/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from `myprojtest-439206`.`Test`.`my_first_dbt_model`
-where id = 1
